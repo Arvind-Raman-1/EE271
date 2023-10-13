94ad64deef788b146efdec4ade95da1e2cacde75
ramanarv <ramanarv@uw.edu>
2023-10-13 14:17:25 -0700

    Final lab update

--- lab1a/DE1_SoC.sv
+++ lab1a/DE1_SoC.sv
@@ -1,3 +1,85 @@
+/*module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, KEY, LEDR, SW);
+
+    output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4;
+    output logic [4:0] LEDR; // 5 LEDs for Sum3 to Sum0 and Cout
+    input logic [3:0] KEY;
+    input logic [8:0] SW; // 9 switches (SW0 for Cin, SW4-SW1 for A, and SW8-SW5 for B)
+    
+    fullAdder FA (
+        .A(SW[8:5]),  // Bits 8 to 5 (SW8-SW5) for B
+        .B(SW[4:1]),  // Bits 4 to 1 (SW4-SW1) for A
+        .cin(SW[0]),  // Bit 0 (SW0) for Cin
+        .sum(LEDR[4:0]),  // 5 LEDs for Sum3 to Sum0
+        .cout(LEDR[5])  // LED for Cout
+    );
+    
+    assign HEX0 = 7'b1111111;
+    assign HEX1 = 7'b1111111;
+    assign HEX2 = 7'b1111111;
+    assign HEX3 = 7'b1111111;
+    assign HEX4 = 7'b1111111;
+    
+endmodule
+
+
+module DE1_SoC_testbench();
+
+    logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4;
+    logic [4:0] LEDR;
+    logic [3:0] KEY;
+    logic [8:0] SW; // 9 switches (SW0 for Cin, SW4-SW1 for A, and SW8-SW5 for B)
+    
+    DE1_SoC dut (
+        .HEX0(HEX0),
+        .HEX1(HEX1),
+        .HEX2(HEX2),
+        .HEX3(HEX3),
+        .HEX4(HEX4),
+        .KEY(KEY),
+        .LEDR(LEDR),
+        .SW(SW)
+    );
+    
+    integer i;
+    
+    initial begin
+        // Initialize inputs
+        SW[8:5] = 4'b0000; // Initialize B to 0000
+        SW[4:1] = 4'b0000; // Initialize A to 0000
+        SW[0] = 1'b0;       // Initialize Cin to 0
+        
+        // Simulate different inputs
+        for (i = 0; i < 2**9; i = i + 1) begin
+            // Update A, B, and Cin with the current count value
+            SW[8:5] = i[8:5];
+            SW[4:1] = i[4:1];
+            SW[0] = i[0];
+            
+            // Add a delay to observe the output
+            #10;
+            
+            // Display the inputs and outputs
+            $display("SW: %b, Cin: %b, A: %b, B: %b, Sum: %b, Cout: %b",
+                SW, SW[0], SW[4:1], SW[8:5], LEDR, LEDR[5]);
+        end
+        
+        // End simulation
+        $finish;
+    end
+
+endmodule */
+
+
+
+
+
+
+
+
+
+
+
+
 module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 
 	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
@@ -7,14 +89,15 @@ module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 	
 	fullAdder FA (.A(SW[2]), .B(SW[1]), .cin(SW[0]), .sum(LEDR[0]), .cout(LEDR[1]));
 	
-	assign HEX0 = 7'b1111111;
-	assign HEX1 = 7'b1111111;
-	assign HEX2 = 7'b1111111;
-	assign HEX3 = 7'b1111111;
-	assign HEX4 = 7'b1111111;
-	assign HEX5 = 7'b1111111;
+	assign HEX0 = 7'b0010000;
+	assign HEX1 = 7'b1101010;
+	assign HEX2 = 7'b1001111;
+	assign HEX3 = 7'b1000010;
+	assign HEX4 = 7'b1000010;
+	assign HEX5 = 7'b0001000;
 	
 endmodule
+
 
 module DE1_SoC_testbench();
 
--- lab1a/fullAdder4.sv
+++ lab1a/fullAdder4.sv
@@ -1,10 +1,12 @@
 module fullAdder4 (A, B, cin, sum, cout);
-    input logic [3:0] A, B;
+    input logic A[3:0];
+	 input logic B [3:0];
 	 input logic cin;
-    output logic [3:0] sum;
+	 
+	 output logic [3:0]sum;
 	 output logic cout;
-
-    logic [3:0] c;
+	 
+	 logic c[2:0];
 
     fullAdder FA0 (.A(A[0]), .B(B[0]), .cin(cin), .sum(sum[0]), .cout(c[0]));
     fullAdder FA1 (.A(A[1]), .B(B[1]), .cin(c[0]), .sum(sum[1]), .cout(c[1]));
@@ -16,8 +18,11 @@ endmodule
 
 
 module fullAdder4_testbench();
-
-		logic A,B,cin, sum,cout;
+		
+		logic [3:0] sum;
+		logic [3:0] A;
+		logic [3:0] B;
+		logic cin, cout;
 		
 		fullAdder dut (A,B,cin,sum,cout);
 		
@@ -27,10 +32,10 @@ module fullAdder4_testbench();
 		
 			for(i = 0; i < 2 **9; i++) begin 
 			 {A,B,cin} = i; #10;
-			 end
+			end
 			
 
-			end
+		end
 		
 endmodule
 
