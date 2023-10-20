2be5af822e87c47b68bb9ea72e52b58261573e8c
ramanarv <ramanarv@uw.edu>
2023-10-20 15:23:02 -0700

    task1 and 1/2 task2 completed

--- lab2a/DE1_SoC.sv
+++ lab2a/DE1_SoC.sv
@@ -1,110 +1,46 @@
-module DE1_SoC (
-    output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5,
-    output logic [9:0] LEDR,
-    input logic [3:0] KEY,
-    input logic [9:0] SW
-);
+module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
 
-    DiscountedStolenDetector DSD (
-        .U(SW[9]),
-        .P(SW[8]),
-        .C(SW[7]),
-        .M(SW[0]),
-        .discount(),
-        .stolen()
-    );
+output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+output logic [9:0] LEDR;
+input logic [3:0] KEY;
+input logic [9:0] SW;
 
-    // Assign HEX values as before
-    assign HEX0 = 7'b0010000;
-    assign HEX1 = 7'b1101010;
-    assign HEX2 = 7'b1001111;
-    assign HEX3 = 7'b1000010;
-    assign HEX4 = 7'b1000010;
-    assign HEX5 = 7'b0001000;
-    
-    // Assign LEDR based on the Discounted and Stolen signals
-    assign LEDR = {DSD.discount, DSD.stolen, 8'b0};
+logic U, P, C, M, discount, stolen;
+
+assign U = SW[9];
+assign P = SW[8];
+assign C = SW[7];
+assign M = SW[0];
+//assign D = LEDR[1];
+//assign S = LEDR[0];
+
+DiscountedStolenDetector FA (.U(U), .P(P), .C(C), .M(M), .discount(LEDR[1]), .stolen(LEDR[0]));
+
+//assign HEX0 = 7'B0010000;
+//assign HEX1 = 7'B0101011;
+//assign HEX2 = 7'B1111001;
+//assign HEX3 = 7'B0100001;
+//assign HEX4 = 7'B0100001;
+//assign HEX5 = 7'B0001000;
+
 endmodule
 
-module DE1_SoC_testbench();
-    logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
-    logic [9:0] LEDR;
-    logic [3:0] KEY;
-    logic [9:0] SW;
-    
-    DE1_SoC dut (
-        .HEX0(HEX0),
-        .HEX1(HEX1),
-        .HEX2(HEX2),
-        .HEX3(HEX3),
-        .HEX4(HEX4),
-        .HEX5(HEX5),
-        .KEY(KEY),
-        .LEDR(LEDR),
-        .SW(SW)
-    );
-    
-    integer i;
-    initial begin
-        SW[9] = 1'b0;
-        SW[8] = 1'b0;
-        SW[7] = 1'b0;
-        SW[0] = 1'b0;
-        for (i = 0; i < 2**8; i = i + 1) begin
-            SW[7:0] = i;
-            #10;
-        end
-        $finish;
-    end
-endmodule
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*module DE1_SoC (HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, KEY, LEDR, SW);
-
-	output logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
-	output logic [9:0] LEDR;
-	input logic [3:0] KEY;
-	input logic [9:0] SW; 
-	
-	fullAdder FA (.A(SW[2]), .B(SW[1]), .cin(SW[0]), .sum(LEDR[0]), .cout(LEDR[1]));
-	
-	assign HEX0 = 7'b0010000;
-	assign HEX1 = 7'b1101010;
-	assign HEX2 = 7'b1001111;
-	assign HEX3 = 7'b1000010;
-	assign HEX4 = 7'b1000010;
-	assign HEX5 = 7'b0001000;
-	
-endmodule
-
-
+// tests all values of switch combinations which correspond to the input
+// of the 2 4-bit numbers input
 module DE1_SoC_testbench();
 
-	logic [6:0] HEX0,HEX1,HEX2,HEX3,HEX4,HEX5;
-	logic [9:0] LEDR;
-	logic [3:0] KEY;
-	logic [9:0] SW;
-	
-	DE1_SoC dut (.HEX0, .HEX1, .HEX2, .HEX3, .HEX4, .HEX5, .KEY, .LEDR, .SW);
-	
-	integer i;
-	initial begin
-	SW[9] = 1'b0;
-	SW[8] = 1'b0;
-	for(i = 0; i < 2**8; i++) begin
-		SW[7:0] = i; #10;
-	end
+logic [6:0] HEX0, HEX1, HEX2, HEX3, HEX4, HEX5;
+logic [9:0] LEDR;
+logic [3:0] KEY;
+logic [9:0] SW;
+
+DE1_SoC dut (.HEX0, .HEX1, .HEX2, .HEX3, .HEX4, .HEX5, .KEY, .LEDR, .SW);
+
+integer i;
+initial begin
+for (i=0; i<2**4; i++) begin
+{SW[9:7], SW[0]} = i; #10;
+end
 end
 
-endmodule*/
+endmodule
--- lab2a/simulation/modelsim/Lab2project_run_msim_rtl_verilog.do.bak11
+++ lab2a/simulation/modelsim/Lab2project_run_msim_rtl_verilog.do.bak11
@@ -5,5 +5,6 @@ if {[file exists rtl_work]} {
 vlib rtl_work
 vmap work rtl_work
 
+vlog -sv -work work +incdir+C:/Users/ramanarv/Desktop/EE271/lab2a {C:/Users/ramanarv/Desktop/EE271/lab2a/DE1_SoC.sv}
 vlog -sv -work work +incdir+C:/Users/ramanarv/Desktop/EE271/lab2a {C:/Users/ramanarv/Desktop/EE271/lab2a/DiscountedStolenDetector.sv}
 
