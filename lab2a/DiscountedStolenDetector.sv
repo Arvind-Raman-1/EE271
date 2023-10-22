e6ed53f955077e64996dd78f3906e5a1d088ffed
ramanarv <ramanarv@uw.edu>
2023-10-21 17:49:11 -0700

    lab2 finished

--- lab2a/DiscountedStolenDetector.sv
+++ lab2a/DiscountedStolenDetector.sv
@@ -1,46 +1,32 @@
-module DiscountedStolenDetector (U,P,C,M,discount,stolen);
-	 input logic U; 
-    input logic P;  
-    input logic C;  
-	 input logic M;
-    output logic discount;
-    output logic stolen;
+module DiscountedStolenDetector ( U, P, C, M, D, S);
+	input logic U; // Input signal U
+	input logic P; // Input signal P
+	input logic C; // Input signal C
+	input logic M; // Input signal M
+	output logic D; // Output signal D
+	output logic S; // Output signal S
 	
+	// Calculate the Discounted signal D
+	assign D = P | (U & C);  
 	
-	 assign discount = P | (U & C);
-	 assign stolen = ~M & ((U & C) | ~C & (~U | ~P));
+	// Calculate the Stolen signal S
+	assign S = ~M & ((U & C) | ~C & (~U | ~P)); 
 	
-endmodule 
+endmodule
 
-module DiscountedStolenDetector_testbench();
-
-    logic U, P, C, M;
-    logic discount, stolen;
-
-    DiscountedStolenDetector dut (
-        .U(U),
-        .P(P),
-        .C(C),
-        .M(M),
-        .discount(discount),
-        .stolen(stolen)
-    );
-
-    integer i;
-
-    initial begin
-        $display("U P C M | Discount Stolen");
-        $display("------------------------");
-
-        for (i = 0; i < 2**4; i = i + 1) begin
-            {U, P, C, M} = i;
-            #10;
-            $display("%b %b %b %b  |   %b       %b", U, P, C, M, discount, stolen);
-        end
-
-        $finish;
-    end
+module DiscountedStolenDetector_testbench ();
+	logic U, P, C, M, D, S;
+	DiscountedStolenDetector dut (.U(U), .P(P), .C(C), .M(M), .D(D), .S(S)); // Instantiate the DiscountedStolenDetector module
+	
+	integer i;
+	initial begin
+		for (i = 0; i < 2**4; i = i + 1) begin // Iterate through different combinations of input values
+			{U, P, C, M} = i; // Set the input signals based on the current value of 'i'
+			#10; // Wait for a delay of 10 time units
+		end
+		$stop; 
+	end
 
 endmodule
 
-
+		
